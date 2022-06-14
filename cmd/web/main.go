13163a447f4aa6894769a256dffbd51195baff95
akmalsan <akmalssan@gmail.com>
2022-06-14 13:15:48 +0700

    create handler to serve files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,16 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	// Create a file server which servers files from the
+	// "./ui/static" directory. Path to http.Dir function
+	// is relative to the directory root
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	// Use the mux.Handle function to register the file server
+	// as the handler for all URL paths that start with "/static".
+	// Strip the "/static" prefix before it reaches the file server
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// Start the server
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
