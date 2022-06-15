7d2d79785bbc96abd15b8e5a3e06107bd07ba486
akmalsan <akmalssan@gmail.com>
2022-06-15 12:12:21 +0700

    improve logging - add leveled logging to differentiate information & error messages - implement custom error logger instead of using standard logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,6 +12,16 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	// Parse the command-line flags
 	flag.Parse()
+
+	// Use log.New to create a logger for writing info messages.
+	// Three parameters: destination to write logs to, string
+	// prefix for message, and flags to indicate additional
+	// information to include
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	// Create a logger for writing error messages but use stderr
+	// as the destination and use the log.Lshortfile flag to
+	// include the file name and line number in the log message
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Create a new router
 	mux := http.NewServeMux()
@@ -28,8 +39,19 @@ func main() {
 	// Strip the "/static" prefix before it reaches the file server
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// Initialize a new http.Server struct. Set the Addr and Handler
+	// fields so the server uses the same network address and routes as
+	// before. Set the ErrorLog field so the server uses the custom
+	// errorLog logger
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// Value returned from the flag.String function is a pointer
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	// Call the ListenAndServe method on the new http.Server struct
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
